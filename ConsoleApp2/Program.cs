0ccfb0cdc6469356f05ce6490353366b5ddfadd0
Emily <yi.hu17@education.nsw.gov.au>
2023-02-15 10:43:04 +1100

    added extra hello world

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello world");
             Console.ReadLine();
         }
     }
